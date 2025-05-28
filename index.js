156e819e3a279f6b52d98b6e831b5d1991d30ec4
vyshnav <vyshnavajai30@gmail.com>
2025-05-28 09:58:07 +0530

    'vysh'

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer=require("json-server")
+
+ const server=jsonServer.create()
+
+ server.use(jsonServer.defaults())
+ server.use(jsonServer.router("db.json"))
+
+ const PORT=3000
+
+ server.listen(prompt,()=>{
+    console.log("Server running at",PORT)
+ })
